d9fb684482dc3a0ac046a9446f8616a61af55955
Theogene <igiranezatheogene4@gmail.com>
2026-05-05 19:16:57 +0200

    piping

--- streams.js
+++ streams.js
@@ -5,10 +5,14 @@ const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf-8'}) 
 
 const writeStream = fs.createWriteStream('./docs/blog4.txt')
 
-readStream.on('data', (chunk) => { // on is event listener and data is the event name
+// readStream.on('data', (chunk) => { // on is event listener and data is the event name
 
-    console.log('-- NEW CHUNK --')
-    console.log(chunk)
-    writeStream.write('\nNEW CHUNK\n')
-    writeStream.write(chunk)
-})
+//     console.log('-- NEW CHUNK --')
+//     console.log(chunk)
+//     writeStream.write('\nNEW CHUNK\n')
+//     writeStream.write(chunk)
+// })
+
+// PIPING
+
+readStream.pipe(writeStream)
