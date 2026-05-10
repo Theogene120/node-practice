b4ac04af46c6700b64a9880cdb451f041a2e9410
Theogene <igiranezatheogene4@gmail.com>
2026-05-10 18:33:41 +0200

    changes

--- app.js
+++ app.js
@@ -2,29 +2,35 @@ const express = require('express') // this return the function
 
 const app = express() // creating an instance of the function 
 
+// register view engine
+
+app.set('view engine', 'ejs')
+
 // listen to request
 app.listen(3000) // 3000 is the port
 
-app.get('/',(req, res) => {  // get method respond to a get request
-    //res.send('<p>Home Page</p>') // In express no need of specifying the content header
-    res.sendFile('./views/index.html', {root: __dirname})
+app.get('/',(req, res) => { 
+    res.render('index')
 })
 
 // create about page path
 
 app.get('/about',(req, res) => { 
-    //res.send('<p>About Page</p>')
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about')
+})
+
+app.get('/blogs/create',(req, res) => { 
+    res.render('create')
 })
 
 // redirect
 
 app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+    res.redirect('about')
 })
 
 // 404 page
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404')
 })
