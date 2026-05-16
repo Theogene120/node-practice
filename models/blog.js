9bd566d1f2a8048136029879f017a0e6e8010658
Theogene <igiranezatheogene4@gmail.com>
2026-05-16 18:22:20 +0200

    create schema and model of blog

---
+++ models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema()
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+// timestamps: true if for tracking when update, addition or other 
+// changes happened, it is an option
+
+
+// model
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+// export model
+
+module.exports = Blog;
