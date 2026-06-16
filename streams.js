245c09f6084c0d88565187419af1050e5c3dd155
Theogene <igiranezatheogene4@gmail.com>
2026-06-16 12:06:56 +0200

    Refactor comments in streams.js for clarity and consistency

--- streams.js
+++ streams.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf-8'}) // encoding let text comes in readable form
-                                                                            //  instead of using toString()
+
+//  instead of using toString()
 
 const writeStream = fs.createWriteStream('./docs/blog4.txt')
 
 // readStream.on('data', (chunk) => { // on is event listener and data is the event name
 
-//     console.log('-- NEW CHUNK --')
-//     console.log(chunk)
-//     writeStream.write('\nNEW CHUNK\n')
+//     // console.log('-- NEW CHUNK --')
+//     // console.log(chunk)
+//     writeStream.write('\n\n\n\nNEW CHUNK\n\n\n')
 //     writeStream.write(chunk)
 // })
 
